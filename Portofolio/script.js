c1d92848773f2edc384895bd700e793df4fa2964
BitterDuck <luloboy2@gmail.com>
2025-08-26 17:58:23 +0300

    portofolio

---
+++ Portofolio/script.js
@@ -0,0 +1,24 @@
+const element = document.querySelector('.rec') //rectangle
+const button = document.querySelector('.button')
+let isDown = false //bool to check
+button.addEventListener('click', () => { //function
+    //gets current top position
+    let currentTop = parseInt(window.getComputedStyle(element).top);
+    if (isDown == false){ //checks bool
+    element.style.top = (currentTop + 300) + 'px'; //moves down
+    isDown = true;
+    } else {
+        element.style.top = (currentTop - 300) + 'px'; //moves up
+        isDown = false;
+    }
+});
+
+function updateHour(){
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, '0');
+    const min = now.getMinutes().toString().padStart(2, '0');
+    document.getElementById('hour').textContent = hours+':'+min;
+}
+
+setInterval(updateHour,1000)
+updateHour();
