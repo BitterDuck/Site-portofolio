cb20ee9914d720029dbbf35200e4df0907ecd375
BitterDuck <luloboy2@gmail.com>
2025-08-27 00:34:47 +0300

    audio

--- Portofolio/script.js
+++ Portofolio/script.js
@@ -2,20 +2,40 @@ const element = document.querySelector('.rec') //rectangle
 const button = document.querySelector('.button')
 const frame = document.querySelector('.frame')
 const clrbutton = document.getElementById("clrbutton")
-let isDown = false //bool to check
+const musicbutton = document.getElementById("musicbutton")
+const music = document.getElementById('music')
+let started = false;
+let isDown = true; //bool to check
 button.addEventListener('click', () => { //function
     //gets current top position
     let currentTop = parseInt(window.getComputedStyle(element).top);
     if (isDown == false){ //checks bool
-    element.style.top = (currentTop + 300) + 'px'; //moves down
+    element.style.top = (currentTop + 180) + 'px'; //moves down
     isDown = true;
     } else {
-        element.style.top = (currentTop - 300) + 'px'; //moves up
+        element.style.top = (currentTop - 180) + 'px'; //moves up
         isDown = false;
     }
 });
 clrbutton.addEventListener("click", () => {
     frame.classList.toggle("dark");
+})
+
+musicbutton.addEventListener("click", () => {
+    
+    if (!started) {
+        music.volume = 0.35;
+        music.play().then(() => {
+          console.log("Music started");
+          started = true;
+          musicbutton.classList.add("active");
+        }).catch(err => {
+          console.error("Playback error:", err);
+        });
+      } else {
+        music.muted = !music.muted;
+        musicbutton.classList.toggle("active");
+      }
 })
 
 function updateHour(){
